a2257f86e01de007988563fc773eb77b482205ed
Brady Werling <bradywerling@gmail.com>
2024-12-07 16:52:15 -0500

    Added interface files and some queries

---
+++ Interface/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows;
+using System.Data.SqlClient;
+using System.Data;
+using MySqlConnector;
+using System.Data.Common;
+
+namespace WowDB {
+  public partial class MainWindow : Window {
+    MySqlConnection mConnectionMySQL = null; // Create MySQL connection object 
+    MySqlDataAdapter da; // Create data adapter object
+    DataTable dt = new DataTable(); // Create data table object 
+    string connStr = ""; // String to build connection
+    string query = ""; // Stores query text
+
+    public MainWindow () {
+      InitializeComponent();
+    }
+
+    private void btnConnect_Click (object sender, RoutedEventArgs e) {
+        // build the connection string
+        connStr = @"Server=" + "trevor.butler.edu;" +                     // server name
+            "PORT=" + "3306" + ";" +                                      // port number
+            "Database=bwerling;" +                                        // database name
+            "Uid=bwerling;" +                                             // user id
+            "Pwd=Crg2f3EFaJj1HU0w";                                       // password
+        mConnectionMySQL = new MySqlConnection(connStr); // Set connection object using info string
+        lblConnected.Content = "Database connected"; // Show connected in display
+    }
+
+    //-------------------- Change query string for each button --------------------//
+    private void btnQ1_Click (object sender, RoutedEventArgs e) {query = "SELECT * FROM Class;";}
+
+    private void btnQ2_Click (object sender, RoutedEventArgs e){query = "SELECT * FROM Spec;";}
+
+    private void btnQ3_Click (object sender, RoutedEventArgs e) {query = "SELECT * FROM Race;";}
+
+    private void btnQ4_Click (object sender, RoutedEventArgs e) {query = "SELECT class_name AS Class, spec_name AS Specialization, raid_name AS Raid, log_date, percentile_90_damage, percentile_50_damage, percentile_10_damage\r\nFROM Raid_Performance\r\nJOIN Raid\r\n\tON Raid_Performance.raid_id = Raid.raid_id \r\nJOIN Spec\r\n\tON Raid_Performance.spec_id = Spec.spec_id \r\nJOIN Class\r\n\tON Spec.class_id = Class.class_id;"; }
+
+    private void btnQ5_Click (object sender, RoutedEventArgs e) {query = "SELECT * FROM Raid;";}
+
+    private void btnRun_Click (object sender, RoutedEventArgs e) {
+        // If not connected, show user in connection label
+        if (mConnectionMySQL is null) {lblConnected.Content = "Not connected";}
+        // Else query the database
+        else {
+            da = new MySqlDataAdapter(query, mConnectionMySQL); // Send query to database
+            DataTable dt = new DataTable();
+            da.Fill(dt); // Load query results into table
+
+            // ---- Fixes underscores in column names for the table display --- //
+            // Loop through each column in data table
+            foreach (DataColumn column in dt.Columns) {
+	            // If column has an underscore...
+                if (column.ColumnName.Contains("_")) {
+                    column.ColumnName = column.ColumnName.Replace("_", "__"); // Replace it with two underscores
+                }
+            }
+
+            dgResults.ItemsSource = dt.DefaultView;
+        }
+    }
+
+    private void btnDone_Click (object sender, RoutedEventArgs e) {
+        if (mConnectionMySQL != null) mConnectionMySQL.Close(); // Close connection if not already
+        System.Environment.Exit(-1); // Exit the program
+    }
+  }
+}
--- queries.sql
+++ queries.sql
@@ -10,25 +10,35 @@
 SELECT race_name AS Race, spell_name, spell_desc
 FROM Spell s
 JOIN Race_Spell rs
-ON s.spell_id = rs.spell_id 
+	ON s.spell_id = rs.spell_id 
 JOIN Race r
-ON rs.race_id = r.race_id 
+	ON rs.race_id = r.race_id 
 WHERE s.spell_type = 'Race_Spell';
 
 -- Print the name of a class with all its spells and their descriptions
 SELECT class_name AS Class, spell_name, spell_desc
 FROM Spell s
 JOIN Class_Spell cs
-ON s.spell_id = cs.spell_id 
+	ON s.spell_id = cs.spell_id 
 JOIN Class c
-ON cs.class_id = c.class_id 
+	ON cs.class_id = c.class_id 
 WHERE s.spell_type = 'Class_Spell';
 
 -- Print the name of a specialization with all its spells and their descriptions
 SELECT spec_name AS Specialization, spell_name, spell_desc
 FROM Spell s
 JOIN Spec_Spell ss
-ON s.spell_id = ss.spell_id 
+	ON s.spell_id = ss.spell_id 
 JOIN Spec sp
-ON ss.spec_id = sp.spec_id 
+	ON ss.spec_id = sp.spec_id 
 WHERE s.spell_type = 'Spec_Spell';
+
+-- Print dps for each spec
+SELECT class_name AS Class, spec_name AS Specialization, raid_name AS Raid, log_date, percentile_90_damage, percentile_50_damage, percentile_10_damage
+FROM Raid_Performance
+JOIN Raid
+	ON Raid_Performance.raid_id = Raid.raid_id 
+JOIN Spec
+	ON Raid_Performance.spec_id = Spec.spec_id 
+JOIN Class
+	ON Spec.class_id = Class.class_id;
